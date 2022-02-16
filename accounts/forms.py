2572db5a50266ee8323cd6b1084f44269103771b
amir <amir@email.com>
2022-02-16 12:08:57 +0330

    add ep4

---
+++ accounts/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+class UserCreationForm(forms.ModelForm):
+	password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+	password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput)
+
+	class Meta:
+		model = User
+		fields = ('email', 'phone_number', 'full_name')
+
+	def clean_password2(self):
+		cd = self.cleaned_data
+		if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+			raise ValidationError('passwords dont match')
+		return cd['password2']
+
+	def save(self, commit=True):
+		user = super().save(commit=False)
+		user.set_password(self.cleaned_data['password1'])
+		if commit:
+			user.save()
+		return user
+
+
+class UserChangeForm(forms.ModelForm):
+	password = ReadOnlyPasswordHashField(help_text="you cant change password using <a href=\"../password/\">this form</a>.")
+
+	class Meta:
+		model = User
+		fields = ('email', 'phone_number', 'full_name', 'password', 'last_login')
