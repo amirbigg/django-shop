ff739f4b82a7fbec878c863f4e047e16d8a465ec
amir <amir@email.com>
2022-02-17 12:32:30 +0330

    add ep8

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, VerifyCodeForm
 import random
 from utils import send_top_code
-from .models import OtpCode
+from .models import OtpCode, User
 from django.contrib import messages
 
 
@@ -32,8 +32,26 @@ class UserRegisterView(View):
 
 
 class UserRegisterVerifyCodeView(View):
+	form_class = VerifyCodeForm
+
 	def get(self, request):
-		pass
+		form = self.form_class
+		return render(request, 'accounts/verify.html', {'form':form})
 
 	def post(self, request):
-		pass
+		user_session = request.session['user_registration_info']
+		code_instance = OtpCode.objects.get(phone_number=user_session['phone_number'])
+		form = self.form_class(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
+			if cd['code'] == code_instance.code:
+				User.objects.create_user(user_session['phone_number'], user_session['email'],
+										 user_session['full_name'], user_session['password'])
+
+				code_instance.delete()
+				messages.success(request, 'you registered.', 'success')
+				return redirect('home:home')
+			else:
+				messages.error(request, 'this code is wrong', 'danger')
+				return redirect('accounts:verify_code')
+		return redirect('home:home')
