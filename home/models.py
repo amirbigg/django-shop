607628b9a25d2e12d48ee82ca85b713e7eceb220
amir <amir@email.com>
2022-02-21 11:24:22 +0330

    add ep11

--- home/models.py
+++ home/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=200, unique=True)
+
+	class Meta:
+		ordering = ('name',)
+		verbose_name = 'category'
+		verbose_name_plural = 'categories'
+
+	def __str__(self):
+		return self.name
+
+
+class Product(models.Model):
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+	name = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=200, unique=True)
+	image = models.ImageField(upload_to='products/')
+	description = models.TextField()
+	price = models.IntegerField()
+	available = models.BooleanField(default=True)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ('name',)
+
+	def __str__(self):
+		return self.name
+
+
+
