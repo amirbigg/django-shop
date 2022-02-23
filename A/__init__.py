acacaebab5123e8128a878f69ae1d10afd790814
amir <amir@email.com>
2022-02-23 11:38:45 +0330

    add ep15

---
+++ A/__init__.py
@@ -0,0 +1,1 @@
+from .celery_conf import celery_app
---
+++ A/celery_conf.py
@@ -0,0 +1,18 @@
+from celery import Celery
+from datetime import timedelta
+import os
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A.settings')
+
+celery_app = Celery('A')
+celery_app.autodiscover_tasks()
+
+celery_app.conf.broker_url = 'amqp://rabbitmq'
+celery_app.conf.result_backend = 'rpc://'
+celery_app.conf.task_serializer = 'json'
+celery_app.conf.result_serializer = 'pickle'
+celery_app.conf.accept_content = ['json', 'pickle']
+celery_app.conf.result_expires = timedelta(days=1)
+celery_app.conf.task_always_eager = False
+celery_app.conf.worker_prefetch_multiplier = 4
