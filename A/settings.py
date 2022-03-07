ee2dd614f8e7aa368ee233ec7a29a19c1ffc0d25
amir <amir@email.com>
2022-03-07 18:14:40 +0330

    add ep32

--- A/settings.py
+++ A/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'orders.context_processors.cart',
             ],
         },
     },
--- orders/cart.py
+++ orders/cart.py
@@ -22,6 +22,9 @@ class Cart:
 			item['total_price'] = int(item['price']) * item['quantity']
 			yield item
 
+	def __len__(self):
+		return sum(item['quantity'] for item in self.cart.values())
+
 	def add(self, product, quantity):
 		product_id = str(product.id)
 		if product_id not in self.cart:
