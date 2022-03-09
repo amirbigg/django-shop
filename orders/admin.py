15eaf4970165127f85265b0b04120f80be198e23
amir <amir@email.com>
2022-03-09 17:26:27 +0330

    add ep33

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+	model = OrderItem
+	raw_id_fields = ('product',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user', 'updated', 'paid')
+	list_filter = ('paid',)
+	inlines = (OrderItemInline,)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from home.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+	user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='orders')
+	paid = models.BooleanField(default=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ('paid', '-updated')
+
+	def __str__(self):
+		return f'{self.user} - {self.id}'
+
+	def get_total_price(self):
+		return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+	order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	price = models.IntegerField()
+	quantity = models.IntegerField(default=1)
+
+	def __str__(self):
+		return self.id
+
+	def get_cost(self):
+		return self.price * self.quantity
